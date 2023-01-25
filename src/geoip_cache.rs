07bab915b1ddb68e0fd6a926e8d5471d2818386b
Piotr Olszewski <asmie@asmie.pl>
2023-01-25 12:19:38 +0100

    chore: fmt cleaning (to be squashed)

--- src/geoip_cache.rs
+++ src/geoip_cache.rs
@@ -6,7 +6,7 @@ use std::time::SystemTime;
 use std::{collections::HashMap, fs, io, time::Duration};
 use tokio::sync::RwLock;
 
-use ziggurat_core_geoip::geoip::{GeoInfo, GeoIPService};
+use ziggurat_core_geoip::geoip::{GeoIPService, GeoInfo};
 
 #[derive(Clone, Serialize, Deserialize)]
 struct CachedIp {
