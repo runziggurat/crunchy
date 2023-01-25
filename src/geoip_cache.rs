2fa7a08245a0388ddd116d951db249f358a77e88
Piotr Olszewski <asmie@asmie.pl>
2023-01-25 12:58:20 +0100

    feat: geoip cache and ip localization info included in state.json (#2)
    This commit introduces GeoIP functionality (geo-cache, location info in state.json).
    Attention: state.json has changed its structure and now has all geoip fields included.
    
    Additionally:
    - uplifted spectre, added updated deps
    - doc: added deps.rs badge

---
+++ src/geoip_cache.rs
@@ -0,0 +1,109 @@
+use serde::{Deserialize, Serialize};
+use std::net::IpAddr;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::time::SystemTime;
+use std::{collections::HashMap, fs, io, time::Duration};
+use tokio::sync::RwLock;
+
+use ziggurat_core_geoip::geoip::{GeoIPService, GeoInfo};
+
+#[derive(Clone, Serialize, Deserialize)]
+struct CachedIp {
+    pub last_updated: SystemTime,
+    pub info: GeoInfo,
+}
+
+#[derive(Default, Clone, Serialize, Deserialize)]
+struct GeoCache {
+    pub entries: HashMap<IpAddr, CachedIp>,
+}
+
+/// GeoIP cache responsible for getting and caching results.
+pub struct GeoIPCache {
+    /// Available providers and their configuration.
+    providers: Vec<Box<dyn GeoIPService>>,
+    /// Path to the cache file.
+    cache_file: PathBuf,
+    /// Cache entries.
+    cache: Arc<RwLock<GeoCache>>,
+}
+
+impl GeoIPCache {
+    /// Create a new GeoIP cache.
+    pub fn new(cache_file: &Path) -> Self {
+        Self {
+            providers: Vec::new(),
+            cache_file: cache_file.to_owned(),
+            cache: Arc::new(RwLock::new(GeoCache::default())),
+        }
+    }
+
+    /// Add a new provider to the list of providers. The providers will be called in the order they
+    /// are added.
+    pub fn add_provider(&mut self, provider: Box<dyn GeoIPService>) {
+        self.providers.push(provider);
+    }
+
+    /// Load the cache from the file.
+    pub async fn load(&self) -> Result<(), io::Error> {
+        let cache_string = fs::read_to_string(&self.cache_file)?;
+
+        let mut cache = self.cache.write().await;
+        cache.entries = serde_json::from_str(&cache_string).unwrap();
+        Ok(())
+    }
+
+    /// Save the cache to the file.
+    pub async fn save(&self) -> Result<(), io::Error> {
+        let cache = self.cache.read().await;
+        let cache_string = serde_json::to_string(&cache.entries).unwrap();
+        fs::write(&self.cache_file, cache_string)
+    }
+
+    /// Function look in cache and if not found, it will call the providers to fetch new data and
+    /// store it into cache.
+    pub async fn lookup(&self, ip: IpAddr) -> Option<GeoInfo> {
+        if let Some(info) = self.check_cache(ip).await {
+            return Some(info);
+        }
+
+        for provider in self.providers.iter() {
+            let entry = provider.lookup(ip).await;
+            if let Ok(ip_geo_info) = entry {
+                let mut rw_cache = self.cache.write().await;
+                let cache_entry = CachedIp {
+                    last_updated: SystemTime::now(),
+                    info: ip_geo_info.geo_info,
+                };
+                rw_cache.entries.insert(ip, cache_entry.clone());
+                return Some(cache_entry.info);
+            }
+        }
+
+        None
+    }
+
+    async fn check_cache(&self, ip: IpAddr) -> Option<GeoInfo> {
+        let mut remove_entry = false;
+
+        {
+            let cache = self.cache.read().await;
+            let res = cache.entries.get(&ip);
+            if let Some(entry) = res {
+                // Check if the entry is not too old.
+                if entry.last_updated.elapsed().unwrap() < Duration::from_secs(60 * 60 * 24 * 14) {
+                    return Some(entry.info.clone());
+                }
+                remove_entry = true;
+            }
+        }
+
+        if remove_entry {
+            let mut rw_cache = self.cache.write().await;
+            rw_cache.entries.remove(&ip);
+        }
+
+        None
+    }
+}
